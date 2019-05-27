1b9de7e5a952eda3df30c8d0f3528715cf8064ed
Alex Olar <olaralex666@gmail.com>
2019-05-27 12:16:11 +0200

    removed printing

--- detectron/roi_data/fast_rcnn.py
+++ detectron/roi_data/fast_rcnn.py
@@ -223,13 +223,11 @@ def _expand_bbox_targets(bbox_target_data):
         num_bbox_reg_classes = 2  # bg and fg
 
     clss = bbox_target_data[:, 0]
-    print("CLASS : ", clss)
     bbox_targets = blob_utils.zeros((clss.size, 4 * num_bbox_reg_classes))
     bbox_inside_weights = blob_utils.zeros(bbox_targets.shape)
     inds = np.where(clss > 0)[0]
     for ind in inds:
         cls = int(clss[ind])
-        print("CLS : ", cls)
         start = 4 * cls
         end = start + 4
         bbox_targets[ind, start:end] = bbox_target_data[ind, 1:]
